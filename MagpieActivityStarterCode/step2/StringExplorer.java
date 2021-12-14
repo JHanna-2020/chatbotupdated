9127f9fd8ecae325cc54a192e5720f9f92d07140
John Hanna <john497dit@hotmail.com>
2021-12-14 12:43:05 -0600

    update 12-14

--- MagpieActivityStarterCode/step2/StringExplorer.java
+++ MagpieActivityStarterCode/step2/StringExplorer.java
@@ -14,7 +14,17 @@ public class StringExplorer
 		//  Demonstrate the indexOf method.
 		int position = sample.indexOf("quick");
 		System.out.println ("sample.indexOf(\"quick\") = " + position);
-		
+
+		//	personal added tests
+		int notFoundPsn = sample.indexOf("slow");
+		System.out.println("sample.indexOf(\"slow\") = "+notFoundPsn );
+
+		//demostrates two pramater indexOf()
+		int startPos = sample.indexOf("quick",10);
+		System.out.println("sample.indexOf(\"quick\",10) = "+startPos);
+		//^ returns -1 because "quick" is before the index 10 which is where it starts to looks
+
+
 		//  Demonstrate the toLowerCase method.
 		String lowerCase = sample.toLowerCase();
 		System.out.println ("sample.toLowerCase() = " + lowerCase);
